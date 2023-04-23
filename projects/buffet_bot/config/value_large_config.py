81fe9ecaa471f711a1f310b8414dbd63271b77d9
Michael Liang <michaelliang15@gmail.com>
2023-04-23 15:33:37 +1000

    Added sharpe ratios

---
+++ projects/buffet_bot/config/value_large_config.py
@@ -0,0 +1,24 @@
+from ml_collections import ConfigDict
+
+
+def get_config():
+    config = ConfigDict()
+
+    config.investor_type = "value_large"
+    config.initial_investment = 100_000
+    config.context_window_date = "2018-01-01"
+    config.investment_schedule = "monthly"
+    config.num_simulated_months = 48
+    config.num_simulations = 10
+    config.llm_additional_context = "news"
+    config.experiment_folder_path = (
+        "output/experiments/news_context_ss_200_filtered_value_large"
+    )
+    config.additional_context_dataset_path = (
+        "context_data/huff_news_with_impact_scores.json"
+    )
+    config.additional_context_sample_size = (
+        200  # Only used if llm_additional_context == "news"
+    )
+    config.transaction_cost = 0.0001  # TODO implement
+    return config
