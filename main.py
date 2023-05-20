6319d86b341e2c86dd4e29067f52faefe5c5d39c
Michael Liang <michaelliang15@gmail.com>
2023-05-20 13:34:21 +1000

    clean up comments

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import silicron
 
+# Set key
 API_KEY = "your_api_key_here"
 bot = silicron.Silicron(API_KEY)
 
 # Upload data
-# data_file_paths = "./tests/data/test.txt"
-# bot.upload_data(data_file_paths, index_name="test-index")
+data_file_paths = "./tests/data/test.txt"
+bot.upload_data(data_file_paths, index_name="test-index")
 
-
+# Get response
 prompt = "Who is Michael Liang?"
-
 config = {"chatbot": None, "database": "test-index"}
-
 response = bot.get_response(prompt, config=config)
 
 print(response)
