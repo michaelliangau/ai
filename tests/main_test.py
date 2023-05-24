919a1287a8d9be8805a2a7ed413e15312a6f8973
Michael Liang <michaelliang15@gmail.com>
2023-05-24 16:59:49 +1000

    add todo

--- tests/main_test.py
+++ tests/main_test.py
@@ -7,6 +7,7 @@ from main import app
 
 client = TestClient(app)
 
+# TODO: Make tests more flushed out once the API is more flushed out
 def test_read_root():
     response = client.get("/hello")
     assert response.status_code == 200
