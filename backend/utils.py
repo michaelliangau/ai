19128f726981954b15c8a0c9f2b1c5034c8b763b
Michael Liang <michaelliang15@gmail.com>
2023-06-01 17:38:36 +1000

    black format

--- backend/utils.py
+++ backend/utils.py
@@ -2,6 +2,7 @@ import openai
 import pinecone
 import boto3
 
+
 def get_embedding(text: str, model: str = "text-embedding-ada-002"):
     """
     Get embeddings for the given text using the specified OpenAI model.
