b19b4f55c87d81bb6053e65076fa8513c8aac08e
bouahth2018 <bouahth2018@gmail.com>
2023-06-02 16:32:21 +0700

    Change background colour

--- frontend/app/(marketing)/layout.tsx
+++ frontend/app/(marketing)/layout.tsx
@@ -10,7 +10,7 @@ interface MarketingLayoutProps {
 
 export default function MarketingLayout({ children }: MarketingLayoutProps) {
   return (
-    <div className="bg-neutral-900">
+    <div className="bg-neutral-950">
       <Header />
       {children}
     </div>
