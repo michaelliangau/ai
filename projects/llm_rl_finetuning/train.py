ce4a2190de5ce588d1d367847592f1e8c8bfd394
Michael Liang <michaelliang15@gmail.com>
2023-08-31 23:37:53 +1000

    Add dataset

--- projects/llm_rl_finetuning/train.py
+++ projects/llm_rl_finetuning/train.py
@@ -1,4 +1,5 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import datasets
 import torch
 import agent
 import environment
@@ -14,26 +15,36 @@ model = GPT2LMHeadModel.from_pretrained('gpt2')
 env = environment.Environment(max_seq_length=max_seq_length)
 ppo_agent = agent.PPOAgent(model, tokenizer)
 
+# Dataset
+huggingface_dataset = datasets.load_dataset('squad')
+questions = []
+for example in huggingface_dataset['train']:
+    question = example['question']
+    questions.append(question)
+
 # Train loop
 for epoch in range(epochs):
-    generated_sequence = env.reset()
-    log_probs = []
-    rewards = []
+    for question in questions:
+        print(f'Epoch {epoch}: Question {question}')
+        generated_sequence = tokenizer.encode(question, return_tensors='pt')
+        log_probs = []
+        rewards = []
 
-    # Generate sequence
-    for _ in range(env.max_seq_length):
-        IPython.embed()
-        action, log_prob = ppo_agent.select_action(generated_sequence)
-        reward, done = env.step(action)
-        log_probs.append(log_prob)
-        rewards.append(reward)
-        generated_sequence = torch.cat((generated_sequence, torch.tensor([action])))
-        if done:
-            break
+        # Generate sequence
+        for _ in range(env.max_seq_length):
+            action, log_prob = ppo_agent.select_action(generated_sequence)
+            IPython.embed()
+            # TODO working through this to give the model a reward and then update the model based on the reward.
+            reward, done = env.step(action)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+            generated_sequence = torch.cat((generated_sequence, torch.tensor([action])))
+            if done:
+                break
 
-    # Compute loss and update policy
-    loss = ppo_agent.compute_loss(log_probs, rewards)
-    ppo_agent.optimizer.zero_grad()
-    loss.backward()
-    ppo_agent.optimizer.step()
-    print(f'Epoch {epoch}: Loss {loss.item()}')
+        # Compute loss and update policy
+        loss = ppo_agent.compute_loss(log_probs, rewards)
+        ppo_agent.optimizer.zero_grad()
+        loss.backward()
+        ppo_agent.optimizer.step()
+        print(f'Epoch {epoch}: Loss {loss.item()}')
