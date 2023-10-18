7f8094bc9f6b4f396a7b9695be702c4dc87283bc
Michael Liang <michaelliang15@gmail.com>
2023-10-18 08:49:21 +0000

    tokenizer

--- projects/denoising_diffusion_primitives/train.py
+++ projects/denoising_diffusion_primitives/train.py
@@ -1,6 +1,5 @@
 import datasets
-import IPython
-from PIL import Image
+import transformers
 import torchvision
 import model
 
@@ -9,12 +8,24 @@ forward_beta = 0.2
 forward_num_timesteps = 100
 forward_decay_rate = 0.98
 
+# Tokenizer
+tokenizer = transformers.T5TokenizerFast.from_pretrained("t5-small")
+text_embedding_model = transformers.T5EncoderModel.from_pretrained("t5-small")
+text = "Hello my name is Michale"
+
+inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+outputs = text_embedding_model(**inputs)
+text_embedding = outputs.last_hidden_state
+average_text_embedding = text_embedding.mean(dim=1)
+
+# TODO: This is just toy code above tokenizer stuff.
+
 # Data
 ds = datasets.load_dataset('HuggingFaceM4/COCO', '2014_captions', split='test') # TODO: Don't use the test split
 transform = torchvision.transforms.ToTensor()
 image = transform(ds[0]["image"])
 text = ds[0]["sentences_raw"][0]
-import IPython; IPython.embed()
+
 # Data Loader
 
 
