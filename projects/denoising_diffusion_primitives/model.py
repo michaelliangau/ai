841f12202297f6bc9561a0c16a94033f991f279f
Michael Liang <michaelliang15@gmail.com>
2023-10-18 06:26:27 +0000

    forward noising works

--- projects/denoising_diffusion_primitives/model.py
+++ projects/denoising_diffusion_primitives/model.py
@@ -1,7 +1,46 @@
 import torch
-
+import numpy as np
 
 class ForwardProcess():
+    """Adds noise to an image in a forward process."""
+    def __init__(self, num_timesteps=100, initial_beta=0.2, decay_rate=0.98):
+        """Initialize the forward process.
+
+        Args:
+            num_timesteps: Number of timesteps in the diffusion process.
+            initial_beta: Initial beta value. This is a hyperparameter that we tune.
+                It represents what is the standard deviation of the noise that we add to
+                the images at the first timestep (which has maximum noise).
+            decay_rate: Decay rate for each subsequent beta.
+        """
+        self.betas = self.generate_betas(num_timesteps, initial_beta, decay_rate)
+    
+    def generate_betas(self, num_timesteps, initial_beta, decay_rate):
+        """Generate an array of betas for diffusion.
+        
+        Q: Why is betas going from high values to low?
+        A: It follows the timesteps of the backward process which starts from lots of
+            noise and gradually removes noise.
+        
+        Args:
+            num_timesteps: Number of timesteps in the diffusion process.
+            initial_beta: Initial beta value.
+            decay_rate: Decay rate for each subsequent beta.
+            
+        Returns:
+            A torch.Tensor containing generated betas.
+        """
+        betas = [initial_beta]
+        for _ in range(1, num_timesteps):
+            next_beta = betas[-1] * decay_rate
+            betas.append(next_beta)
+        
+        # Normalize betas so their sum is 1 (optional)
+        betas = np.array(betas)
+        betas /= np.sum(betas)
+
+        return torch.tensor(betas)
+    
     def sample(self, image, timestep):
         """Sample from the forward process at a specific timestep.
         
@@ -9,7 +48,6 @@ class ForwardProcess():
             image: The image to noise.
             timestep: The timestep to sample at.
         """
-        # Add Gaussian noise to the image
-        noise = torch.randn_like(image)
-        noised_image = image + noise * timestep # TODO: I don't think timestep can be multipled like that but maybe it can at it's simplest?
-        return noised_image
+        noise_std = torch.sqrt(self.betas[timestep])
+        noise = torch.randn_like(image) * noise_std
+        return image + noise
--- projects/denoising_diffusion_primitives/train.py
+++ projects/denoising_diffusion_primitives/train.py
@@ -5,19 +5,23 @@ import torchvision
 import model
 
 # Hyperparameters
+forward_beta = 0.2
+forward_num_timesteps = 100
+forward_decay_rate = 0.98
 
 # Data
 ds = datasets.load_dataset('HuggingFaceM4/COCO', '2014_captions', split='test') # TODO: Don't use the test split
-
-# transform = torchvision.transforms.ToTensor()
-# tensor_image = transform(ds[0]["image"])
-# ds[0]["image"].save("original_image.jpg")
+import IPython; IPython.embed()
+transform = torchvision.transforms.ToTensor()
+image = transform(ds[0]["image"])
 
 # Data Loader
 
 
 # Forward Noising Step
-# TODO: Build the Forward Noisng Step
+forward_process = model.ForwardProcess(num_timesteps=forward_num_timesteps, initial_beta=forward_beta, decay_rate=forward_decay_rate)
+noised_image = forward_process.sample(image, timestep=0)
+
 
 
 # Backward Generation Step
