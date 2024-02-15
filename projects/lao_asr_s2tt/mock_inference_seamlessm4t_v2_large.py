943aafa32e547de7bf1c33413d0d7b3615591d51
Michael Liang <michaelliang15@gmail.com>
2024-02-15 14:01:47 +0000

    trying to fix large inference

--- projects/lao_asr_s2tt/mock_inference_seamlessm4t_v2_large.py
+++ projects/lao_asr_s2tt/mock_inference_seamlessm4t_v2_large.py
@@ -1,13 +1,20 @@
+import torch
 from transformers import AutoProcessor, SeamlessM4Tv2Model
 import torchaudio
+device = torch.device("cuda:0")
 
 processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
-model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
-
+model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large").to(device)
 # from audio
 audio, orig_freq =  torchaudio.load("/home/ubuntu/ai/projects/lao_asr_s2tt/test_data/test_lao_short.wav")
-audio =  torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000) # must be a 16 kHz waveform array
-audio_inputs = processor(audios=audio, return_tensors="pt")
-audio_array_from_audio = model.generate(**audio_inputs, tgt_lang="eng")[0].cpu().numpy().squeeze()
+import IPython; IPython.embed()
+audio =  [torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000)] # must be a 16 kHz waveform array
+inputs = processor(audios=audio, return_tensors="pt", sampling_rate=16_000)
+inputs = {k: v.to(device) for k, v in inputs.items()}
+output_tokens = model.generate(**inputs, tgt_lang="eng")
 
-# TODO: Verify good performance on seamlessv2large model... Odd that I was getting different results in the streaming setting, wondering what's going on. SM4t medium seems to work
+# TODO: Inference isn't working, need to fix this...weird.
+output_tokens = output_tokens[0][0]
+decoded_texts = [processor.decode(output_token, skip_special_tokens=True) for output_token in output_tokens]
+
+print(decoded_texts)
