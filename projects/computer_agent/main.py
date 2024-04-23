6e759209a539c5518d410047fd35c5473d5de143
Michael Liang <michaelliang15@gmail.com>
2024-04-23 02:29:33 +0000

    added gacc

--- projects/computer_agent/main.py
+++ projects/computer_agent/main.py
@@ -9,7 +9,7 @@ import PIL
 import os
 
 # Load the dataset
-ds = datasets.load_from_disk('data/red_dot_dataset_single_sample')
+ds = datasets.load_from_disk('data/red_dot_dataset')
 train_test_split = ds.train_test_split(test_size=0.1)
 train_ds = train_test_split['train']
 test_ds = train_test_split['test']
@@ -40,7 +40,7 @@ def collate_fn(batch):
 # Define training arguments
 training_args = transformers.TrainingArguments(
     output_dir='./results',
-    num_train_epochs=10,
+    num_train_epochs=100,
     per_device_train_batch_size=2,
     warmup_ratio=0.1,
     weight_decay=0.005,
@@ -52,6 +52,8 @@ training_args = transformers.TrainingArguments(
     save_total_limit=10,
     load_best_model_at_end=True,
     dataloader_num_workers=os.cpu_count(),  # Set the number of workers to the number of CPUs
+    gradient_accumulation_steps=4,
+    fp16=True,
 )
 
 # Initialize the Trainer with the collate_fn
